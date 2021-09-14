1bd62b184669fbf11444b7d6d6028cc0b6407d03
alifahokta <alifahokta1@gmail.com>
2021-09-14 13:13:49 +0700

    Kode Program Kedua

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        echo "NIM  : 2031710095 <br>";
+        echo "Nama : Alifah Okta Nur Wardani";
+    }
+}
